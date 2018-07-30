911fc15507976ad3c4edf010d1bb1362692b09ac
Moises Macero <mechero@users.noreply.github.com>
2018-07-30 07:22:38 +0200

    Removed unused import

--- src/test/java/com/thepracticaldeveloper/objectmapperbasics/ObjectMapperBasicsTest.java
+++ src/test/java/com/thepracticaldeveloper/objectmapperbasics/ObjectMapperBasicsTest.java
@@ -25,7 +25,6 @@ import org.slf4j.LoggerFactory;
 import java.io.IOException;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
-import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.TreeMap;
